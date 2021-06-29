3f162050fd615629b0eae0dcbdf430e17183b8fa
Fabricio Andrade <fabriciosaand@gmail.com>
2021-06-29 15:59:07 -0300

    get authenticated user

--- src/main/java/com/fabriciosaand/contact/controller/ContactController.java
+++ src/main/java/com/fabriciosaand/contact/controller/ContactController.java
@@ -5,8 +5,11 @@ import com.fabriciosaand.contact.requests.ContactPostRequestBody;
 import com.fabriciosaand.contact.requests.ContactPutRequestBody;
 import com.fabriciosaand.contact.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/contact")
 @RequiredArgsConstructor
+@Log4j2
 public class ContactController {
 
     private final ContactService contactService;
@@ -29,7 +33,9 @@ public class ContactController {
     }
 
     @PostMapping
-    public ResponseEntity<Contact> save(@RequestBody ContactPostRequestBody contactPostRequestBody){
+    public ResponseEntity<Contact> save(@RequestBody ContactPostRequestBody contactPostRequestBody,
+                                        @AuthenticationPrincipal UserDetails userDetails){
+        log.info(userDetails);
         return new ResponseEntity<>(contactService.save(contactPostRequestBody), HttpStatus.CREATED);
     }
 
