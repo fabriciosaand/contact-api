c04f590b532896587948c96e353eac94411392fe
Fabricio Andrade <fabriciosaand@gmail.com>
2021-06-29 16:13:25 -0300

    adding login form

--- src/main/java/com/fabriciosaand/contact/config/SecurityConfig.java
+++ src/main/java/com/fabriciosaand/contact/config/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                .anyRequest()
                .authenticated()
                .and()
+               .formLogin()
+               .and()
                .httpBasic();
     }
 
